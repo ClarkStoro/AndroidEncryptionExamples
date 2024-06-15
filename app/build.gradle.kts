c3b913272395efaa95068f01c3bbbfc955f14be1
Emanuele Maso <emanuele.maso97@gmail.com>
2024-06-15 14:51:05 +0200

    Upgrade to v2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.clarkstoro.androidencryptionexamples"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
